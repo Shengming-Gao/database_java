2179173c92962cba14a266587eb4d16f72c14b28
Yi <62568965+S3mile@users.noreply.github.com>
2022-11-14 11:49:48 -0500

    part 1 mostly done

--- src/main/java/edu/virginia/cs/hw6/ConfigSingleton.java
+++ src/main/java/edu/virginia/cs/hw6/ConfigSingleton.java
@@ -1,5 +1,11 @@
 package edu.virginia.cs.hw6;
 
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+
 public class ConfigSingleton {
     private static final String configurationFileName = "config.json";
     private static ConfigSingleton instance;
@@ -32,5 +38,26 @@ public class ConfigSingleton {
 
     private void setFieldsFromJSON() {
         //TODO: Population the three fields from the config.json file
+        try {
+            String fileName = "config.json";
+            BufferedReader br = new BufferedReader(new FileReader(fileName));
+            StringBuilder sb = new StringBuilder();
+            String line = br.readLine();
+            while (line != null) {
+                sb.append(line).append('\n');
+                line = br.readLine();
+            }
+            String text = sb.toString();
+
+            JSONObject o = new JSONObject(text);
+
+            busStopsURL = (String) o.get("stops");
+            busLinesURL = (String) o.get("lines");
+            databaseName = o.getString("database");
+        }
+        catch (Exception e) {
+
+        }
+
     }
 }
